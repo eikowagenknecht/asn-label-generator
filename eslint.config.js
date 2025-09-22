ddf90774b2eb1a402183d4e283689c9955884612
Eiko Wagenknecht <git@eiko-wagenknecht.de>
2025-09-22 11:19:32 +0200

    chore: update tselint config
    Signed-off-by: Eiko Wagenknecht <git@eiko-wagenknecht.de>

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 // Common
-import globals from "globals";
+
 // Typescript
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
